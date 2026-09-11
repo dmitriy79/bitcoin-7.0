9777723bff264e9348a54a28255c325bf4ef993d
dmitriy79 <sinoptik79@inbox.ru>
2026-09-12 08:16:20 +1000

    Update net.h

--- src/net.h
+++ src/net.h
@@ -315,7 +315,7 @@ public:
         // We're using mapAskFor as a priority queue,
         // the key is the earliest time the request can be sent
         int64& nRequestTime = mapAlreadyAskedFor[inv];
-        if (fDebugNet)
+   //     if (fDebugNet)
           //  printf("askfor %s   %"PRI64d" (%s)\n", inv.ToString().c_str(), nRequestTime, DateTimeStrFormat("%H:%M:%S", nRequestTime/1000000).c_str());
 
         // Make sure not to reuse time indexes to keep things in the same order
